e0705d21d9580d73340200e7b40459eda3a679d4
luca sicoli <luca.sicoli12@hotmail.com>
2024-06-03 17:35:25 -0300

    imagenes actualizadas

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       title: "Migrating to Linear 101",
       summary: "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get started.",
       tags: ["Design", "Research"],
-      imageUrl: "./images/migrating.jpg",  // Asegúrate de tener una imagen real aquí
+      imageUrl: "./images/post-1.png",  // Asegúrate de tener una imagen real aquí
       author: "Phoenix Baker",
       date: "19 Jan 2022"
     },
@@ -23,7 +23,7 @@ function App() {
       title: "UX review presentations",
       summary: "How do you create compelling presentations that wow your colleagues and impress your managers?",
       tags: ["Design", "Research", "Presentation"],
-      imageUrl: "./images/ux.jpg",  // Asegúrate de tener una imagen real aquí
+      imageUrl: "./images/post-2.png",  // Asegúrate de tener una imagen real aquí
       author: "Olivia Rhye",
       date: "20 Jan 2022"
     }
@@ -32,26 +32,26 @@ function App() {
   const podcastEpisodes = [
     {
       id: 1,
-      title: "Expanding beyond service management",
-      description: "Episode 05 with Suresh GP discussing expanding beyond traditional service management roles.",
+      title: "Bill Wall Leadership Lessons",
+      description: "Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?.",
       tags: ["Leadership", "Management"],
-      image: "./images/optimizacion.png",  // Asegúrate de tener una imagen real aquí
+      image: "./images/card-1.jpeg",  // Asegúrate de tener una imagen real aquí
       date: "17 Jan 2022"
     },
     {
       id: 2,
-      title: "The face of service: Support managers need inspiration too",
-      description: "Episode 08 with Megan Engels on the emotional and professional support managers need.",
+      title: "PM mental models",
+      description: "Mental models are simple expressions of complex processes or relationships.",
       tags: ["Product", "Research", "Frameworks"],
-      image: "./images/resources.jpg",  // Asegúrate de tener una imagen real aquí
+      image: "./images/card-2.jpeg",  // Asegúrate de tener una imagen real aquí
       date: "16 Jan 2022"
     },
     {
       id: 3,
-      title: "ESM and the importance of proper communication",
-      description: "Episode 07 with Kevin Clark discussing the importance of communication in ESM.",
+      title: "What is Wireframing?",
+      description: "introduction to Wireframing and its principles. learn from the best in the insdustry",
       tags: ["Design", "Research"],
-      image: "./images/comunication.jpg",  // Asegúrate de tener una imagen real aquí
+      image: "./images/card-3.jpeg",  // Asegúrate de tener una imagen real aquí
       date: "15 Jan 2022"
     }
   ];
